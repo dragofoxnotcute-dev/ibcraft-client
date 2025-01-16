bff59360c2108c347aef8e3124f2d361a60ee033
Sylvester Alderson <51517881+SylvesterFox@users.noreply.github.com>
2025-01-16 18:15:39 +0300

    Commit

---
+++ ib-craft-frontend/src/pages/HomePage.tsx
@@ -0,0 +1,21 @@
+import CardBlock from "../components/CardBlock/CardBlock";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import MainBlock from "../components/MainBlock/MainBlock";
+import QuestionsBlock from "../components/questionsBlock/QuestionsBlock";
+import TitleBlock from "../components/titleBlock/TitleBlock";
+
+function HomePage() {
+    return(
+        <>
+            <Header/>
+            <MainBlock/>
+            <TitleBlock/>
+            <CardBlock/>
+            <QuestionsBlock/>
+            <Footer/>
+        </>
+    )
+}
+
+export default HomePage;
