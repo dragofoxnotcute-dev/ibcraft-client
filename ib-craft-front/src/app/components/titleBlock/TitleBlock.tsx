7241fb143fb1451479cec908b0570f1452548ab2
Sylvester Alderson <51517881+SylvesterFox@users.noreply.github.com>
2025-03-11 17:55:58 +0300

    Skin animation

--- ib-craft-front/src/app/components/titleBlock/TitleBlock.tsx
+++ ib-craft-front/src/app/components/titleBlock/TitleBlock.tsx
@@ -2,29 +2,52 @@ import style from './titleblock.module.css'
 import moderator_skin from "@static/ib_craft_moderader.svg"
 import ilyaskin from "@static/ib_craft_owner.svg"
 import Image from "next/image";
+import { useEffect, useRef, useState } from 'react';
 
 function TitleBlock () {
+
+    const sectionRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsVisible(entry.isIntersecting);
+            },
+            { threshold: 0.3 }
+        );
+
+        if (sectionRef.current) {
+            observer.observe(sectionRef.current);
+        }
+
+        return () => {
+            if (sectionRef.current) {
+                observer.unobserve(sectionRef.current);
+            }
+        };
+
+    }, []);
+
     return (
         <div className={style.main_title}>
-            <div className={style.title_block}>
-                
-                <div className={style.text_container}>
-                    <span className={`${style.character} ${style.creator}`}>
+            <section ref={sectionRef} className={style.character_container}>
+                <div className={`${style.character} ${isVisible ? style.visible : style.hidden}  ${style.creator} `}>
                             <Image src={ilyaskin} alt="andery"/>
-                    </span>
-                   <div className={style.block_context_title}>
-                        <p className={style.text}>
-                            Вы устали от игры на серверах с модами или от игры на серверах,
-                            созданных только для доната? Хочется спокойно поиграть в ванильный
-                            Minecraft без лишней суеты с небольшим количеством РП в мультиплеере?
-                            Тогда мы рады вам представить наш сервер на версии 1.20.1 - IB-Craft!
-                        </p>
-                    </div>
-                    <span className={`${style.character} ${style.moderator}`}>
-                        <Image src={moderator_skin} alt="ilyabot"/>
-                    </span>
                 </div>
-                
+                <div className={`${style.character}  ${isVisible ? style.visible : style.hidden}  ${style.moderator}`}>
+                        <Image src={moderator_skin} alt="ilyabot"/>
+                </div>
+            </section>
+            <div className={style.text_container}>
+                <div className={style.block_context_title}>
+                    <p className={style.text}>
+                        Вы устали от игры на серверах с модами или от игры на серверах,
+                        созданных только для доната? Хочется спокойно поиграть в ванильный
+                        Minecraft без лишней суеты с небольшим количеством РП в мультиплеере?
+                        Тогда мы рады вам представить наш сервер на версии 1.20.1 - IB-Craft!
+                    </p>
+                </div>
             </div>
         </div>
     ) 
