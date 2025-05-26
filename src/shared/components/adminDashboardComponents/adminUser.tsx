6bbed57d2f96d0371c1c9db5b8b7bc27b385e6d1
DragofoxNotCute <kit28nik@gmail.com>
2025-05-26 19:49:30 +0300

    nav bar admin

--- src/shared/components/adminDashboardComponents/adminUser.tsx
+++ src/shared/components/adminDashboardComponents/adminUser.tsx
@@ -7,7 +7,47 @@ export default function AdminUsers() {
             <div className="container">
                 <div className={style.main_users}>
                     <div className={style.UsersBlock}>
-                        <DevBlock height={"120vh"} />
+                        <nav className={style.bartools}>
+                            <ul className={style.items_tools}>
+                                <li className={style.list_item}>
+                                    <a href="">
+                                        < i className={`bx  bx-trash-alt ${style.icon}`}  ></i> 
+                                        <span>Добавить</span>
+                                        <span>пользователя</span>
+                                    </a>
+                                </li>
+                                <li className={style.list_item}>
+                                    <a href="">
+                                        <i className={`bx  bx-plus-circle ${style.icon}`}></i> 
+                                        <span>Удалить</span>
+                                        <span>выбраное</span>
+                                    </a>
+                                </li>
+                                <li className={style.list_item}>
+                                    <a href="">
+                                        < i className={`bx  bx-search-alt ${style.icon}`}  ></i>  
+                                        <span>Поиск</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <ul className={style.pages}>
+                                <li className={style.pages_item}>
+                                    <a href="" className={`${style.nav_link} ${style.disable}`}>
+                                        < i className='bx  bx-chevron-left'  ></i> 
+                                    </a>
+                                </li>
+                                <li className={style.pages_title}>
+                                    1 из 1
+                                </li>
+                                <li className={style.pages_item}>
+                                    <a href="" className={style.nav_link}>
+                                        < i className='bx  bx-chevron-right'  ></i> 
+                                    </a>
+                                </li>
+                                       
+                            </ul>
+                        </nav>
+                        {/* <DevBlock height={"120vh"} /> */}
                     </div>
                 </div>
             </div>
