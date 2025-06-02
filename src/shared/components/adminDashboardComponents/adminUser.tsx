5941235366f6b80467e69f497020af46d91ae5aa
DragofoxNotCute <kit28nik@gmail.com>
2025-06-03 02:06:49 +0300

    upd

--- src/shared/components/adminDashboardComponents/adminUser.tsx
+++ src/shared/components/adminDashboardComponents/adminUser.tsx
@@ -1,5 +1,7 @@
-import DevBlock from "../development/Devblock"
+import Image from "next/image"
+import AdminNavUser from "./adminNavUser"
 import style from "./adminUser.module.css"
+import user_ico from "@static/user.svg"
 
 export default function AdminUsers() {
     return (
@@ -7,47 +9,19 @@ export default function AdminUsers() {
             <div className="container">
                 <div className={style.main_users}>
                     <div className={style.UsersBlock}>
-                        <nav className={style.bartools}>
-                            <ul className={style.items_tools}>
-                                <li className={style.list_item}>
-                                    <a href="">
-                                        <i className={`bx  bx-plus-circle ${style.icon}`}></i> 
-                                        <span>Добавить</span>
-                                        <span>пользователя</span>
-                                    </a>
-                                </li>
-                                <li className={style.list_item}>
-                                    <a href="">
-                                        < i className={`bx  bx-trash-alt ${style.icon}`}  ></i>
-                                        <span>Удалить</span>
-                                        <span>выбраное</span>
-                                    </a>
-                                </li>
-                                <li className={style.list_item}>
-                                    <a href="">
-                                        < i className={`bx  bx-search-alt ${style.icon}`}  ></i>  
-                                        <span>Поиск</span>
-                                    </a>
-                                </li>
-                            </ul>
-                            <ul className={style.pages}>
-                                <li className={style.pages_item}>
-                                    <a href="" className={`${style.nav_link} ${style.disable}`}>
-                                        < i className='bx  bx-chevron-left'  ></i> 
-                                    </a>
-                                </li>
-                                <li className={style.pages_title}>
-                                    1 из 1
-                                </li>
-                                <li className={style.pages_item}>
-                                    <a href="" className={style.nav_link}>
-                                        < i className='bx  bx-chevron-right'  ></i> 
-                                    </a>
-                                </li>
-                                       
-                            </ul>
-                        </nav>
-                        {/* <DevBlock height={"120vh"} /> */}
+                        <AdminNavUser />
+                        <ul className={style.itemsListUser}>
+                            <li>
+                                <input type="checkbox" name="" id="" />
+                                <Image src={user_ico} width={25} height={27} alt="user" />
+                                <a href="" className={style.user_name}>IlyaBot</a>
+                            </li>
+                            <li>
+                                <input type="checkbox" name="" id="" />
+                                <Image src={user_ico} width={25} height={27} alt="user" />
+                                <a href="" className={style.user_name}>Dragofox</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
